a7c5d492acf0fab2eccdce6cb85fc8ef29e83ea3
Guilherme Fernandes <84087774+Gui28F@users.noreply.github.com>
2022-12-02 10:50:05 +0000

    added a test to check if the translation was being done correctly

--- ganttproject-tester/test/net/sourceforge/ganttproject/test/task/TestTaskInfoImp.java
+++ ganttproject-tester/test/net/sourceforge/ganttproject/test/task/TestTaskInfoImp.java
@@ -16,6 +16,7 @@ import biz.ganttproject.core.time.CalendarFactory;
 import biz.ganttproject.core.time.GanttCalendar;
 import biz.ganttproject.core.time.TimeDurationImpl;
 import net.sourceforge.ganttproject.TestSetupHelper;
+import net.sourceforge.ganttproject.language.GanttLanguage;
 import net.sourceforge.ganttproject.task.TaskImpl;
 import net.sourceforge.ganttproject.task.TaskInfo;
 import net.sourceforge.ganttproject.task.TaskManager;
@@ -24,6 +25,7 @@ import net.sourceforge.ganttproject.task.Task;
 import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 
 public class TestTaskInfoImp extends TaskTestCase {
@@ -61,7 +63,7 @@ public class TestTaskInfoImp extends TaskTestCase {
         String info = "<html>Name: task_1<br>" +
                 "Duration: 2 days<br>" +
                 "Completion Percentage: 0%<br>" +
-                "Remaining Time: 0 days<br> " +
+                "Remaining Time: 0 days<br>" +
                 "Priority: normal<br>" +
                 "Notes: <b></b></html>";
         assertEquals(info, taskInfo.getTaskMainInfoHTML());
@@ -76,7 +78,7 @@ public class TestTaskInfoImp extends TaskTestCase {
         String info = "<html>Name: task_1<br>" +
                 "Duration: 4 days<br>" +
                 "Completion Percentage: 0%<br>" +
-                "Remaining Time: 4 days<br> " +
+                "Remaining Time: 4 days<br>" +
                 "Priority: normal<br>" +
                 "Notes: <b></b></html>";
         assertEquals(info, taskInfo.getTaskMainInfoHTML());
@@ -92,7 +94,7 @@ public class TestTaskInfoImp extends TaskTestCase {
         String info = "<html>Name: task_1<br>" +
                 "Duration: 4 days<br>" +
                 "Completion Percentage: 0%<br>" +
-                "Remaining Time: 4 days<br> " +
+                "Remaining Time: 4 days<br>" +
                 "Priority: normal<br>" +
                 "Notes: test notes<b></b></html>";
         assertEquals(info, taskInfo.getTaskMainInfoHTML());
@@ -114,7 +116,7 @@ public class TestTaskInfoImp extends TaskTestCase {
         String info = "<html>Name: task_1<br>" +
                 "Duration: 4 days<br>" +
                 "Completion Percentage: 0%<br>" +
-                "Remaining Time: 4 days<br> " +
+                "Remaining Time: 4 days<br>" +
                 "Priority: normal<br>" +
                 "Notes: test notes test notes test notes test<br>notes test notes test notes test notes<br>" +
                 "test notes test notes test notes test<br>notes test notes test notes test notes<br>" +
@@ -135,7 +137,7 @@ public class TestTaskInfoImp extends TaskTestCase {
         String info = "<html>Name: task_2<br>" +
                 "Duration: 1 days<br>" +
                 "Completion Percentage: 20%<br>" +
-                "Remaining Time: 1 days<br> " +
+                "Remaining Time: 1 days<br>" +
                 "Priority: normal<br>" +
                 "Notes: test notes<b></b></html>";
         assertEquals(info, taskInfo.getTaskMainInfoHTML());
@@ -158,9 +160,38 @@ public class TestTaskInfoImp extends TaskTestCase {
         String info = "<html>Name: task_2<br>" +
                 "Duration: 4 days<br>" +
                 "Completion Percentage: 20%<br>" +
-                "Remaining Time: 3 days<br> " +
+                "Remaining Time: 3 days<br>" +
                 "Priority: normal<br>" +
                 "Notes: test notes<b></b></html>";
+        assertEquals(info, taskInfo.getTaskMainInfoHTML());
+    }
+
+    public void testGetTaskMainInfoHTML7() {
+        GanttLanguage.getInstance().setLocale(new Locale("pt_PT"));
+        Task task1 = createTask();
+        task1.setStart(getNextDate(Calendar.TUESDAY));
+        task1.setDuration(getTaskManager().createLength(4));
+        task1.setName("task_1");
+        task1.setNotes("test notes test notes test notes test notes test notes test notes test notes test notes " +
+                "test notes test notes test notes test notes test notes test notes test notes test notes test notes " +
+                "test notes test notes test notes test notes test notes test notes test notes test notes test " +
+                "notes test notes test notes test notes test notes test notes test notes test notes test notes test " +
+                "notes test notes test notes test notes test notes test notes test notes test notes test notes test " +
+                "notes test notes test notes test notes test notes test notes test notes test notes test notes test " +
+                "notes test notes test notes test notes test notes test notes test notes test notes ");
+        TaskInfo taskInfo = task1.getTaskInfo();
+        String info = "<html>Nome: task_1<br>" +
+                "Duracao: 4 dias<br>" +
+                "Percentagem Concluida: 0%<br>" +
+                "Tempo Restante: 4 dias<br>" +
+                "Prioridade: normal<br>" +
+                "Notas: test notes test notes test notes test<br>notes test notes test notes test notes<br>" +
+                "test notes test notes test notes test<br>notes test notes test notes test notes<br>" +
+                "test notes test notes test notes test<br>notes test notes test notes test notes<br>" +
+                "test notes test notes test notes test<br>notes test notes test notes test notes<br>" +
+                "test notes test notes test notes test<br>notes test notes test notes test notes<br>" +
+                "test notes test notes test notes test<br>notes test notes test notes test notes<br>" +
+                "test notes test notes test notes test <br><b>mais...</b></html>";
         assertEquals(info, taskInfo.getTaskMainInfoHTML());
     }
 
